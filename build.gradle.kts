c483e2d8ed101fe8ef2936dfdc23289c10d51ec5
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2023-11-17 23:59:51 +0330

    add dependency to app

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+    libs.plugins.apply {
+        alias(androidApplication) apply false
+        alias(kotlinAndroid) apply false
+        alias(hilt.android) apply false
+        alias(ksp) apply false
+    }
 }
 true // Needed to make the Suppress annotation work for the plugins block
