69f5a278bc3aa60dd336702dbb964730580127d6
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2023-11-19 13:16:11 +0330

    create BookDetailScreen

--- app/src/main/java/ir/codroid/books/navigation/NavGraph.kt
+++ app/src/main/java/ir/codroid/books/navigation/NavGraph.kt
@@ -3,8 +3,11 @@ package ir.codroid.books.navigation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import ir.codroid.books.presentation.book_detail.BookDetailRoute
 import ir.codroid.books.presentation.book_list.BookListRoute
 
 @ExperimentalMaterial3Api
@@ -15,10 +18,22 @@ fun SetupNavGraph(navController: NavHostController) {
 
         }
         composable(route = Screen.BookList.route) {
-            BookListRoute()
+            BookListRoute(
+                navController = navController
+            )
         }
-        composable(route = Screen.BookDetail.route) {
-
+        composable(route = Screen.BookDetail.route + "?bookId={bookId}", arguments = listOf(
+            navArgument("bookId") {
+                type = NavType.StringType
+                defaultValue = ""
+            }
+        )) { navBackStack ->
+            navBackStack.arguments?.getString("bookId")?.let { bookId ->
+                BookDetailRoute(
+                    navController = navController,
+                    bookId = bookId
+                )
+            }
         }
     }
 }
